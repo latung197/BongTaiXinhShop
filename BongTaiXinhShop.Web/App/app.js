a4280ad14442a85ad1bdcc122ecb8f5a5e92de9c
DESKTOP-9F5APHK\Admin <levantung0000@gmail.com>
2023-10-30 23:41:24 +0700

    a

--- BongTaiXinhShop.Web/App/app.js
+++ BongTaiXinhShop.Web/App/app.js
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="../assets/admin/libs/angular/angular.min.js" />
+
+(function () {
+    angular.modul('btsShop', []).config(config);
+
+    config.$inject=['$stateProvider','$urlRouterProvider']
+
+
+    function config($stateProvider, $urlRouterProviderLV2022) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/App/components/Home/homeView.html",
+            controller:"HomeController"
+
+        })
+    }
+})
